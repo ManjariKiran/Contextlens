984ffe02bed875d31c7dc8a07a4cff2bfed285c5
Manjari KIran <56753695+ManjariKiran@users.noreply.github.com>
2022-11-30 10:18:21 -0500

    Update makepackage.R
    Link on how to set git on Rstudio

--- R/makepackage.R
+++ R/makepackage.R
@@ -2,3 +2,9 @@ install.packages("devtools")
 install.packages("roxygen2")
 devtools::create("contextlens")
 devtools::document()
+
+
+#Connect to github
+https://inbo.github.io/git-course/course_rstudio.html
+
+
