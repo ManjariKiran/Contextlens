f13cf507225e6a6f417579b12d308218b3dfe02c
Manjari Kiran <56753695+ManjariKiran@users.noreply.github.com>
2022-12-15 15:12:13 -0500

    Add files via upload
    Working code

--- R/simulate_loops.R
+++ R/simulate_loops.R
@@ -75,141 +75,38 @@ for(i in 1:21){
     decay_matrix[i,j] <- loopCount_APA[i,j]/loopCount_APA[center,center]
   }
 }
-decay_matrix
 
-##Plot decay loop
-dist_diag <- function(buffer, loop, res, exclude){
-  res <- 10000
-  p <- pairdist(loop)
-  bin <- p/res
-  res1 <- res/1000
-  start <- (bin)*res1
-  m=(buffer*2)+1
-  M <- matrix(data=NA,nrow=m,ncol=m)
-  for(k in 1:m){
-    rem = k-1
-    for(i in k:m){
-      j = i-rem
-      M[i,j] <- start
-    }
-    start <- start - res1
+m=(buffer*2)+1
+center <- buffer+1
+decay_matrix_subs <- matrix(data=NA,nrow=m,ncol=m)
+for(i in 1:21){
+  for(j in 1:21){
+    decay_matrix_subs[i,j] <- loopCount_APA[i,j]/loopCount_APA[center,center]
   }
-  start <- (bin)*res1
-  for(k in 1:m){
-    rem = k-1
-    for(j in k:m){
-      i = j-rem
-      M[i,j] <- start
-    }
-    start <- start + res1
-  }
-  #  M[is.na(M)] <- 1
-  M[M < exclude] <- NA
-  return(M)
-}
-mh_index <- function(buffer, loop, inner, exclude){
-  m=(buffer*2)+1
-  center <- buffer+1
-  M <- matrix(data=NA,nrow=m,ncol=m)
-  for(j in 1:m){
-    l=buffer+j
-    for(i in 1:m){
-      k=(m+1)-j
-      if((i <= (buffer+1)) && (j <= (buffer+1))){
-        M[i,j]<-k-i
-      }
-      if((i <= (buffer+1)) && (j > (buffer+1))){
-        M[i,j]<-j-i
-      }
-      if((i > (buffer+1)) && (j <= (buffer+1))){
-        M[i,j]<-i-j
-      }
-      if((i > (buffer+1)) && (j > (buffer+1))){
-        M[i,j]<-i-k
-      }
-    }
-  }
-  d <- dist_diag(buffer, loops[i], res, exclude)
-  inner_val <- which(M == inner) #Extract MH distance same as inner from M matrix
-  inner_d <- which(!is.na(d)) #Extract MH distance same as inner from d matrix
-  notNA <- intersect(inner_d,inner_val)
-  new <- loop[notNA]
-  return(new)
 }
 
-normalized=c()
-  for(i in 0:10){
-    new <- median(mh_index(buffer = 10, loop = decay_matrix , inner = i,exclude=0))
-    normalized <- append(normalized,new)
-  }
+#foo <- round(decay_matrix[7:15,7:15],2)
 
+decay_matrix_subs <- (1-decay_matrix)
+original <- 100 - (100*decay_matrix_subs)
+sides <- median((c(original[1,],original[,1],original[m,],original[,m])),na.rm=TRUE)
+alpha = (200 - sides)/(100 - sides)
 
+decay_matrix_subs
+TRT <- round(200 - (100*decay_matrix_subs*alpha))
+WT <- 100 - (100*decay_matrix_subs)
 
-#Convert data by setting 100 kb as 0 (subtract 100kb value from all values) and center pixel as 1 (divide all values to center pixel)
-zero = c()
-
-  for(j in 1:11){
-    a <- normalized[j]-normalized[11]
-    b <- normalized[1]-normalized[11]
-    c <- round((a/b),2)
-    zero <- append(zero,c)
-  }
-zero
-install.packages("igraph")
-library(igraph)
-x=c(0:10)
-plot(x,normalized,type=c("l"),ylim=c(0,1),xlab="Radial Distance",ylab="Decay",lwd=4,
-      col="Grey",cex=2.0)
-plot(log(x),log(normalized),type=c("l"),xlab="log (Radial Distance)",ylab="log (Decay)",lwd=4,col="Grey")
-fit_power_law(normalized)
-
-g <- barabasi.game(1000) # increase this number to have a better estimate
-d <- degree(g, mode="in")
-fit1 <- fit_power_law(d+1, 10)
-install.packages("poweRlaw")
-library(poweRlaw)
-xmin = 1; alpha = 1.5
-x = seq(xmin, 10, length.out=100)
-plot(x, dplcon(x, 0.3512009, 3.30777), type="l")
-dplcon(x, 0.3512009, 3.30777)
-##Check with direction
-normalized=data.frame()
-for(i in 1:4){
-for(j in 1:10){
-  normalized[i,j] <- radial_direction(buffer = 10, loop = decay_matrix ,direction =i, inner =j)
+WT <- decay_matrix*100
+TRT <- decay_matrix*200
+normalized1=c()
+normalized2=c()
+for(i in 0:10){
+  new1 <- median(mh_index(buffer = 10, loop = WT , inner = i,exclude=0))
+  new2 <- median(mh_index(buffer = 10, loop = TRT , inner = i,exclude=0))
+  normalized1 <- append(normalized1,new1)
+  normalized2 <- append(normalized2,new2)
 }
-}
-colnames(normalized) <- c("0","1","2","3","4","5","6","7","8","9","10")
 
-radial_direction(buffer,decay_matrix, 1, 1)
-decay_matrix
-#mh_angle(3,l,direction=90:180)
-
-## Using blockApply
-
-countMatrix_obs <-
-  assay(loopCounts) |>
-  aperm(c(3,4,1,2))
-
-spacings <- dim(countMatrix_obs)
-nBlocks <- 20
-spacings[3] <- ceiling(dim(countMatrix_obs)[3] / nBlocks)
-
-grid <- RegularArrayGrid(dim(countMatrix_obs), spacings)
-
-ans <-
-  lapply(1:10, \(i) {
-    tmp <-
-      blockApply(x = countMatrix_obs,
-                 FUN = \(x) apply(x,c(3,4),FUN = \(z) {
-                   median(mh_index(buffer = buffer, loop=z, inner=i, exclude= 0))
-                 }),
-                 grid = grid,
-                 verbose = TRUE,
-                 BPPARAM = MulticoreParam())
-
-    do.call(rbind, tmp)
-  })
 
 observed <- as.matrix(countMatrix_obs[11,11,,])
 as.vector(countMatrix_obs[11,11,1,])
@@ -232,7 +129,7 @@ for(i in 1:length(number)){
   ref[i,] <- cbind(number[i],round(y,3))
 }
 head(observed_stat)
-(ref)
+
 number
 ##Print random numbers for given mean and std
 center_df <- data.frame("rep1","rep2","rep3","rep4","rep5","rep6")
@@ -250,45 +147,47 @@ head(center_df2)
 ##Create 10% fold change loops
 de <- sample(1:10000,size = 1000, replace=F)
 length(de)
+write.csv(de,"total_de")
 de_half <- sample(de,size=400,replace=F)
 length(de_half)
+write.csv(de_half,"total_de_half")
 de_rem <- de[!de %in% de_half]
 length(de_rem)
 de_two <- sample(de_rem,size=300,replace=F)
 length(de_two)
+write.csv(de_two,"total_de_two")
 de_rem <- de_rem[!de_rem %in% de_two]
 length(de_rem)
 de_three <- sample(de_rem,size=200,replace=F)
 length(de_three)
+write.csv(de_three,"total_de_three")
 de_rem <- de_rem[!de_rem %in% de_three]
 length(de_rem)
 de_four <- de_rem
+write.csv(de_four,"total_de_four")
 de_four
-de_four <-(center_df2[de_four,][1:3])
-de_four[1:3] <- apply(de_four[1:3],2,
-                    function(x) as.numeric(as.character(x)))
-de_four <- de_four*4
-de_three <-(center_df2[de_three,][1:3])
-de_three[1:3] <- apply(de_three[1:3],2,
-                      function(x) as.numeric(as.character(x)))
-de_three <- de_three*3
-de_two <-(center_df2[de_two,][1:3])
-de_two[1:3] <- apply(de_two[1:3],2,
-                      function(x) as.numeric(as.character(x)))
-de_two <- de_two*2
-de_half <-(center_df2[de_half,][1:3])
-de_half[1:3] <- apply(de_half[1:3],2,
-                     function(x) as.numeric(as.character(x)))
-de_half <- de_half*1.5
+head(center_df2)
+#de_four <-(center_df2[de_four,][1:3])
+#de_four[1:3] <- apply(de_four[1:3],2,
+#                    function(x) as.numeric(as.character(x)))
+#de_three <-(center_df2[de_three,][1:3])
+#de_three[1:3] <- apply(de_three[1:3],2,
+#                      function(x) as.numeric(as.character(x)))
+#de_two <-(center_df2[de_two,][1:3])
+#de_two[1:3] <- apply(de_two[1:3],2,
+#                      function(x) as.numeric(as.character(x)))
+#de_half <-(center_df2[de_half,][1:3])
+#de_half[1:3] <- apply(de_half[1:3],2,
+#                     function(x) as.numeric(as.character(x)))
 
-
-Remaining <-center_df2[-de,][1:3]
-center_df2 <- rbind(Remaining,de_half,de_two,de_three,de_four)
-treatment <- as.data.frame(center_df2)
-dim(treatment)
-center_df2 <- treatment[order(as.numeric(row.names(center_df2))),]
-head(center_df)
-center_df <- center_df[,1:3]
+#de_half
+#Remaining <-center_df2[-de,][1:3]
+#center_df2 <- rbind(Remaining,de_half,de_two,de_three,de_four)
+#treatment <- as.data.frame(center_df2)
+#dim(treatment)
+#center_df2 <- treatment[order(as.numeric(row.names(center_df2))),]
+#head(center_df)
+#center_df <- center_df[,1:3]
 
 Rep1 <- vector("list",length = 10000)
 Rep2 <- vector("list",length = 10000)
@@ -304,16 +203,12 @@ for(i in 1:10000){
   M4 <- matrix(data=NA,nrow=m,ncol=m)
   M5 <- matrix(data=NA,nrow=m,ncol=m)
   M6 <- matrix(data=NA,nrow=m,ncol=m)
-  for(j in 1:m){
-    for(k in 1:m){
-      M1[j,k] <- round(as.numeric(center_df[i,1])*decay_matrix[j,k])
-      M2[j,k] <- round(as.numeric(center_df[i,2])*decay_matrix[j,k])
-      M3[j,k] <- round(as.numeric(center_df[i,3])*decay_matrix[j,k])
-      M4[j,k] <- round(as.numeric(center_df[i,1])*decay_matrix[j,k])
-      M5[j,k] <- round(as.numeric(center_df[i,2])*decay_matrix[j,k])
-      M6[j,k] <- round(as.numeric(center_df[i,3])*decay_matrix[j,k])
-    }
-  }
+      M1 <- round(as.numeric(center_df[i,1]) - decay_matrix_subs*as.numeric(center_df[i,1]))
+      M2 <- round(as.numeric(center_df[i,2]) - decay_matrix_subs*as.numeric(center_df[i,2]))
+      M3 <- round(as.numeric(center_df[i,3]) - decay_matrix_subs*as.numeric(center_df[i,3]))
+      M4 <- round(as.numeric(center_df[i,4]) - decay_matrix_subs*as.numeric(center_df[i,4]))
+      M5 <- round(as.numeric(center_df[i,5]) - decay_matrix_subs*as.numeric(center_df[i,5]))
+      M6 <- round(as.numeric(center_df[i,6]) - decay_matrix_subs*as.numeric(center_df[i,6]))
 Rep1[[i]] <- M1
 Rep2[[i]] <- M2
 Rep3[[i]] <- M3
@@ -322,7 +217,23 @@ Rep5[[i]] <- M5
 Rep6[[i]] <- M6
 }
 ALL <- list(Rep1,Rep2,Rep3,Rep4,Rep5,Rep6)
-
+de_two
+Rep1[[5913]]
+for(i in 1:length(de_two)){
+  x <- de_two[i]
+  sides <- median((c(Rep1[[x]][1,],Rep1[[x]][,1],Rep1[[x]][m,],Rep1[[x]][,m])),na.rm=TRUE)
+  new_center4 <- 2*as.numeric(center_df[x,4])
+  new_center5 <- 2*as.numeric(center_df[x,5])
+  new_center6 <- 2*as.numeric(center_df[x,6])
+  alpha4 = (new_center4 - sides)/(as.numeric(center_df[x,1]) - sides)
+  alpha5 = (new_center5 - sides)/(as.numeric(center_df[x,2]) - sides)
+  alpha6 = (new_center6 - sides)/(as.numeric(center_df[x,3]) - sides)
+  Rep4[[x]] <- round(new_center4 - (decay_matrix_subs*as.numeric(center_df[x,1])*alpha4))
+  Rep5[[x]] <- round(new_center5 - (decay_matrix_subs*as.numeric(center_df[x,2])*alpha5))
+  Rep6[[x]] <- round(new_center6 - (decay_matrix_subs*as.numeric(center_df[x,3])*alpha6))
+}
+Rep4[[5913]]
+ALL <- list(Rep1,Rep2,Rep3,Rep4,Rep5,Rep6)
 ALL_simulated <- array(unlist(ALL),dim=c(21,21,10000,6))
 ALL <- DelayedArray::DelayedArray(ALL_simulated)
 dim(ALL)
@@ -334,7 +245,7 @@ spacings[3] <- ceiling(dim(ALL)[3] / nBlocks)
 grid <- RegularArrayGrid(dim(ALL), spacings)
 grid
 ans <-
-  lapply(4:6, \(i) {
+  lapply(8:10, \(i) {
     tmp <-
       blockApply(x = ALL,
                  FUN = \(x) apply(x,c(3,4),FUN = \(z) {
@@ -356,18 +267,22 @@ dim(ALL)
 observed <- ALL[11,11,,]
 head(observed)
 
-head(normalized)
-dim(observed)
 ## Isolate count matrix
-
+loop <- data.frame(seq(1:10000))
+loop1 <- cbind(loop,observed)
+colnames(loop1) <- c("loop","WT_1","WT_2","WT_3","TRT_1","TRT_2","TRT_3")
 norm_MH = as.matrix(normalized) / exp(rowMeans(log(as.matrix(normalized))))
 to_remove <- which(!is.finite(rowSums(norm_MH)) == TRUE)
 norm_MH <- norm_MH[-to_remove,]
 observed <- observed[-to_remove,]
 normalized <- normalized[-to_remove,]
 new <- ALL[,,-to_remove,]
+loops <- loop1[-to_remove,]
+l <- subset(loops, loop %in% de_two)
+
 dim(new)
-to_remove
+dim(loops)
+
 ##Add column names
 colnames(observed) <- c("WT_1","WT_2","WT_3","TRT_1","TRT_2","TRT_3")
 colnames(normalized) <- c("WT_1","WT_2","WT_3","TRT_1","TRT_2","TRT_3")
@@ -388,7 +303,7 @@ dds <-
                          design = ~ condition)
 dds$condition <- factor(dds$condition, levels=c("WT","TRT"))
 default <- counts(dds,normalized = FALSE)
-default_sizefactor <- counts(dds, normalized =TRUE)
+#default_sizefactor <- counts(dds, normalized =TRUE)
 sizeFactors(dds)
 head(default_sizefactor)
 ## Run DEseq analysis
@@ -398,7 +313,9 @@ res1 <-
   DESeq(dds) |>
   lfcShrink(coef = "condition_TRT_vs_WT", type="apeglm")
 summary(res1)
-write.csv(res1,"no_normalization.csv")
+file <- cbind(loops,res1)
+head(file)
+write.csv(file,"no_normalization.csv")
 ##With normalization
 dds <-
   DESeqDataSetFromMatrix(countData = round(observed),
@@ -410,81 +327,25 @@ default <- counts(dds,normalized = FALSE)
 head(default)
 default_normfactor <- counts(dds, normalized =TRUE)
 head(default_normfactor)
-head(observed)
+
 # perform enrichments
-#dds <- DESeq(dds,betaPrior=FALSE, fitType="local")
-dds <- DESeq(dds)
+
 dds$condition <- factor(dds$condition, levels=c("WT","TRT"))
 normalizationFactors(dds) <- as.matrix(norm_MH)
 
-dds <- estimateDispersionsGeneEst(dds)
-dispersions(dds) <- mcols(dds)$dispGeneEst
-normalizationFactors(dds) <- as.matrix(norm_MH)
-res1 <-
- # DESeq(dds) |>
-  dds |>
-  lfcShrink(coef = "condition_TRT_vs_WT", type="apeglm")
-?lfcShrink
 resultsNames(dds)
 ## Run DEseq analysis
 res1 <-
-  DESeq(dds) |>
-  lfcShrink(coef = "condition_WT_vs_TRT", type="apeglm")
+  DESeq(dds,fitType='local') |>
+  lfcShrink(coef = "condition_TRT_vs_WT", type="apeglm")
 summary(res1)
+file1 <- cbind(loops,res1)
+head(file1)
+write.csv(file1,"normalization.csv")
+subset(file1, loop %in% de_two) |>
+    subset(log2FoldChange > 1) |>
+    subset(padj < 0.1)
 
-plotDispEsts(dds)
 
-a <- res1[as.numeric(row.names(de_four)),]
-a[a$padj < 0.1,]
-res1[1367,]
 
-x <- c(1,2,3,4,5,6,7,8,9,10)
-y <- c(100,86,78,72,65,59,57,55,52,50)
-z <- c(200,180,160,140,100,90,80,70,60,50)
-x1 <- log(x)
-y1 <- log(y)
-z1 <- log(z)
-plot(x,y,ylim=c(0,250))
-lines(x,z)
-lm(x ~ y)
-lm(x1 ~ z1)
-plot(density(rexp(1000,0.2)))
-y <- sort(round(10*rexp(1000,0.2)),decreasing=T)
-z <- sort(round(10*rexp(1000,0.1)),decreasing=T)
-z
-x <- c(1:1000)
-y1 <- log(y)
-z1 <- log(z)
-plot(x,z,col="red",type="l",lwd=2,xlab="distance",ylab="signal")
-lines(x,y,col=c("black"),lwd=2)
-lm(x ~ y)
-coef(lm(x ~ z))[2]
 
-x=1:20
-y=c(1, 3, 5, 7, 9, 12, 15, 19, 23, 28, 33, 38, 44, 50, 56, 64, 73, 84, 97, 113)
-y1 <- sort(y,decreasing =TRUE)
-z <- 2*y1
-z
-plot(x, y1)
-x
-y1
-model <- lm(log(z)~ x)
-summary(model)
-ln(y) = 5.26776 - 0.20410(x)
-plot(x,log(y1))
-model(10)
-predict(model,10)
-
-ln(y) = 5.26776 - 0.10210()
-
-y=c(1, 3, 5, 7, 9, 12, 15, 19, 23, 28, 33, 38, 44, 50, 56, 64, 73, 84, 97, 113)
-y1 <- sort(y,decreasing=T)
-x <- c(1:20)
-y1
-x
-model <- lm(log(y1)~ x)
-predict(model,data)*5
-data <- data.frame(x,y1)
-data
-plot(predict(model,data))
-?predict
