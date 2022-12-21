5c6fb675beca8e11c425fcf32637406c8727fd58
ManjariKiran <mkiran@github.com>
2022-12-20 22:15:54 -0500

    Recent updates with CNV and true four fold up

--- R/simulate_loops.R
+++ R/simulate_loops.R
@@ -93,35 +93,43 @@ number <- sort(unique(observed_stat[,5]))
 ref <- data.frame("mean","sd")
 colnames(ref) <- c("mean","sd")
 head(ref)
-for(i in 1:length(number)){
+for(i in 2:length(number)){
   x <- mean(observed_stat[which(observed_stat[,5] == number[i]),],na.rm=T)
   y <- sd(observed_stat[which(observed_stat[,5] == number[i]),],na.rm=T)
   ref[i,] <- cbind(number[i],round(y,3))
 }
 
 ##Print random numbers for given mean and std
-center_df <- data.frame("rep1","rep2","rep3","rep4","rep5","rep6","mean")
-colnames(center_df) <- data.frame("rep1","rep2","rep3","rep4","rep5","rep6","mean")
+center_df <- data.frame("rep1","rep2","rep3","rep4","rep5","rep6")
+colnames(center_df) <- data.frame("rep1","rep2","rep3","rep4","rep5","rep6")
 for(i in 1:10000){
-   random <-  sample(number, 1, replace = TRUE)
+   random <-  as.numeric(sample(number, 1, replace = TRUE))
    n_sd <- as.numeric(ref[which(ref$mean == random),]$sd)
-   center_df[i,] <- c(round(rnorm(n=6,mean = random, sd= n_sd),0),random)
+   center_df[i,] <- c(round(rnorm(n=6,mean = random, sd= n_sd),0))
 }
+dat <- as.data.frame(sapply(center_df, as.numeric))
+str(dat)
+WT_mean <- round(rowMeans(dat[,1:3]))
+center_df <- cbind(center_df,WT_mean)
 head(center_df)
-
 ##Select random loops for fold change
 head(center_df)
 center_df2 <- center_df[,4:6]
 head(center_df2)
 ##Create 10% fold change loops
-de <- sample(1:10000,size = 1000, replace=F)
-de_half <- sample(de,size=400,replace=F)
-de_rem <- de[!de %in% de_half]
-de_two <- sample(de_rem,size=300,replace=F)
-de_rem <- de_rem[!de_rem %in% de_two]
-de_three <- sample(de_rem,size=200,replace=F)
-de_rem <- de_rem[!de_rem %in% de_three]
+de <- sample(1:10000,size = 400, replace=F)
+#de_half <- sample(de,size=400,replace=F)
+#de_rem <- de[!de %in% de_half]
+#de_two <- sample(de_rem,size=300,replace=F)
+#de_rem <- de_rem[!de_rem %in% de_two]
+#de_three <- sample(de_rem,size=200,replace=F)
+de_four_cnv <- sample(de,size = 200, replace =F)
+de_rem <- de[!de %in% de_four_cnv]
+#de_rem <- de_rem[!de_rem %in% de_three]
 de_four <- de_rem
+
+write.csv(de_four_cnv,"de_four_cnv")
+write.csv(de_four,"de_four")
 
 ##Set these as center pixel and create local background
 Rep1 <- vector("list",length = 10000)
@@ -146,7 +154,7 @@ for(i in 1:10000){
       M4 <- round(as.numeric(center_df[i,4]) - decay_matrix_subs*as.numeric(center_df[i,4]))
       M5 <- round(as.numeric(center_df[i,5]) - decay_matrix_subs*as.numeric(center_df[i,5]))
       M6 <- round(as.numeric(center_df[i,6]) - decay_matrix_subs*as.numeric(center_df[i,6]))
-      M7 <- round(as.numeric(center_df[i,6]) - decay_matrix_subs*as.numeric(center_df[i,7]))
+      M7 <- round(as.numeric(center_df[i,7]) - decay_matrix_subs*as.numeric(center_df[i,7]))
 Rep1[[i]] <- M1
 Rep2[[i]] <- M2
 Rep3[[i]] <- M3
@@ -158,8 +166,8 @@ Rep7[[i]] <- M7
 ALL <- list(Rep1,Rep2,Rep3,Rep4,Rep5,Rep6)
 length(de_four)
 de_four
-Rep1[[6922]]
-Rep4[[6922]]
+Rep1[[6923]]
+Rep4[[6923]]
 for(i in 1:length(de_four)){
   x <- de_four[i]
   sides <- median(c(Rep7[[x]][1,],Rep7[[x]][,1],Rep7[[x]][m,],Rep7[[x]][,m]),na.rm=TRUE)
@@ -173,8 +181,21 @@ for(i in 1:length(de_four)){
   Rep5[[x]] <- round(new_center5 - (decay_matrix_subs*as.numeric(center_df[x,7])*alpha5))
   Rep6[[x]] <- round(new_center6 - (decay_matrix_subs*as.numeric(center_df[x,7])*alpha6))
 }
-Rep4[[1235]]
+
 ALL <- list(Rep1,Rep2,Rep3,Rep4,Rep5,Rep6)
+
+for(i in 1:length(de_four_cnv)){
+  x <- de_four_cnv[i]
+  new_center4 <- 4*as.numeric(center_df[x,4])
+  new_center5 <- 4*as.numeric(center_df[x,5])
+  new_center6 <- 4*as.numeric(center_df[x,6])
+  Rep4[[x]] <- round(new_center4 - decay_matrix_subs*new_center4)
+  Rep5[[x]] <- round(new_center5 - decay_matrix_subs*new_center5)
+  Rep6[[x]] <- round(new_center6 - decay_matrix_subs*new_center6)
+}
+
+ALL <- list(Rep1,Rep2,Rep3,Rep4,Rep5,Rep6)
+
 ALL_simulated <- array(unlist(ALL),dim=c(21,21,10000,6))
 ALL <- DelayedArray::DelayedArray(ALL_simulated)
 
@@ -218,7 +239,7 @@ observed <- observed[-to_remove,]
 normalized <- normalized[-to_remove,]
 new <- ALL[,,-to_remove,]
 loops <- loop1[-to_remove,]
-l <- subset(loops, loop %in% de_four)
+l <- subset(loops, loop %in% de)
 
 dim(new)
 dim(loops)
@@ -267,16 +288,16 @@ default <- counts(dds,normalized = FALSE)
 head(default)
 default_normfactor <- counts(dds, normalized =TRUE)
 head(default_normfactor)
+head(norm_MH)
 
 # perform enrichments
 
 dds$condition <- factor(dds$condition, levels=c("WT","TRT"))
 normalizationFactors(dds) <- as.matrix(norm_MH)
 
-resultsNames(dds)
 ## Run DEseq analysis
 res1 <-
-  DESeq(dds,fitType='local') |>
+  DESeq(dds) |>
   lfcShrink(coef = "condition_TRT_vs_WT", type="apeglm")
 summary(res1)
 file1 <- cbind(loops,res1)
@@ -285,7 +306,9 @@ write.csv(file1,"normalization.csv")
 subset(file1, loop %in% de_four) |>
     subset(log2FoldChange < 2) |>
     subset(padj > 0.1)
-
-
+subset(file, loop %in% de_four) |>
+  subset(log2FoldChange < 2) |>
+  subset(padj > 0.1)
+center_df[4155,]
 
 
